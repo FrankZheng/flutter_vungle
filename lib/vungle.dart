3614cd9c9a2d205cb355c7bdb50359266be6a74f
Frank Zheng <frank.zheng@vungle.com>
2019-06-14 09:04:20 +0800

    add basic APIs skeleton

--- lib/vungle.dart
+++ lib/vungle.dart
@@ -2,6 +2,19 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+enum UserConsentStatus {
+  Accepted,
+  Denied,
+}
+
+typedef void OnInitilizeListener();
+
+typedef void OnAdPlayableListener(String placementId, bool playable);
+
+typedef void OnAdStartedListener(String placementId);
+
+typedef void OnAdFinishedListener(String placementId, bool isCTAClicked, bool isCompletedView);
+
 class Vungle {
   static const MethodChannel _channel =
       const MethodChannel('vungle');
@@ -10,4 +23,43 @@ class Vungle {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
+  static OnInitilizeListener onInitilizeListener;
+  
+  static OnAdPlayableListener onAdPlayableListener;
+  
+  static OnAdStartedListener onAdStartedListener;
+  
+  static OnAdFinishedListener onAdFinishedListener;
+
+  static void init(String appId) {
+    
+  }
+
+  static void loadAd(String placementId) {
+
+  }
+
+  static void playAd(String placementId) {
+
+  }
+
+  static Future<bool> isAdAvailable(String placementId) async {
+    return false;
+  }
+
+  static void updateConsentStatus(UserConsentStatus status, String consentMessageVersion) {
+
+  }
+
+  static Future<UserConsentStatus> getConsentStatus() async {
+    return null;
+  }
+
+  static Future<String> getConsentMessageVersion() async {
+    return null;
+  }
+  
+
+
 }
